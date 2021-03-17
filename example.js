537fc917b3d9761677b16ff718f6d1c46fb6b9f9
Unitech <alexandre@keymetrics.io>
2021-03-17 19:27:04 +0100

    up

--- example.js
+++ example.js
@@ -12,11 +12,39 @@ serialPort.on('data', function (data) {
 })
 
 mavLink.on('error', function (e) {
-  console.log(e)
+  //console.log(e)
 })
 
+let y_min = null, y_max = null
+let z_min = null, z_max = null
+
 mavLink.on('message', function (message) {
-  console.log(message)
+  if (message._message_name == 'VIBRATION') {
+    console.log(new Date())
+    console.log(message.vibration_x)
+
+
+    if (message.vibration_y < y_min || y_min == null)
+      y_min = message.vibration_y
+    if (message.vibration_y > y_max || y_max == null)  {
+      y_max = message.vibration_y
+      console.log('New y max detected', y_max)
+    }
+
+
+    console.log(message.vibration_y * 100000, y_min * 100000, y_max * 100000)
+
+    if (message.vibration_z < z_min || z_min == null)
+      z_min = message.vibration_z
+    if (message.vibration_z > z_max || z_max == null) {
+      z_max = message.vibration_z
+      console.log('New x max detected', y_max)
+    }
+
+    console.log(message.vibration_z * 100000, z_min * 100000, z_max * 100000)
+
+    //console.log(message.vibration_z * 1000)
+  }
 })
 
 // mavLink.on('Vibration', function (bytes) {
